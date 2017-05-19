928f964f5529f1c55ed825037444305027c24836
Adrian Herrera <adrian.herrera@epfl.ch>
2017-05-19 09:17:42 +0000

    ida: updated highlight_basic_blocks.py script
    Updates for new JSON format. Displays statistics in message window

--- tools/scripts/ida/highlight_basic_blocks.py
+++ tools/scripts/ida/highlight_basic_blocks.py
@@ -3,6 +3,8 @@ Highlights basic blocks that have been covered by S2E (as measured by S2E's
 TranslationBlockCoverage plugin).
 """
 
+from __future__ import division
+
 import json
 import sys
 
@@ -16,12 +18,21 @@ def _color_block(bb, color=0x00ff00):
         idc.SetColor(ea, idc.CIC_ITEM, color)
 
 
+COVERAGE_MESSAGE = 'S2E Basic Block Coverage\n'                             \
+                   '========================\n'                             \
+                   'Total basic blocks: {num_bbs}\n'                        \
+                   'Covered basic blocks: {num_covered_bbs} ({percent:.1%})\n'
+
+
 def basic_block_coverage(json_path):
     bb_coverage = {}
     with open(json_path, 'r') as f:
         bb_coverage = json.load(f)
 
-    for covered_basic_block in bb_coverage:
+    total_bbs = bb_coverage['stats']['total_basic_blocks']
+    covered_bbs = bb_coverage['stats']['covered_basic_blocks']
+
+    for covered_basic_block in bb_coverage['coverage']:
         start_addr = covered_basic_block['start_addr']
 
         func = idaapi.get_func(start_addr)
@@ -33,6 +44,10 @@ def basic_block_coverage(json_path):
         for block in idaapi.FlowChart(func):
             if block.startEA <= start_addr and block.endEA > start_addr:
                 _color_block(block)
+
+    idc.Message(COVERAGE_MESSAGE.format(num_bbs=total_bbs,
+                                        num_covered_bbs=covered_bbs,
+                                        percent=covered_bbs / total_bbs))
 
 
 if __name__ == '__main__':
