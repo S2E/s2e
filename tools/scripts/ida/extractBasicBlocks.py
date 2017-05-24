92cd0357f6d43a2caf386007b53c6823b855145f
Adrian Herrera <adrian.herrera@epfl.ch>
2017-05-24 13:55:43 +0000

    extractBasicBlocks: refactored

--- tools/scripts/ida/extractBasicBlocks.py
+++ tools/scripts/ida/extractBasicBlocks.py
@@ -2,57 +2,58 @@ import idaapi
 import idautils
 import idc
 
-#Flowchart does not consider function calls as basic block boundaries.
-#This function takes a range of addresses and splits additional basic blocks.
-def cls_split_block(fp, startEA, endEA):
-	curName = GetFunctionName(startEA);
-	dem = idc.Demangle(curName, idc.GetLongPrm(INF_SHORT_DN));
-	if dem != None:
-		curName = dem;
-	
-	first=startEA
-	h = idautils.Heads(startEA, endEA)
-	for i in h:
-		mnem = idc.GetMnem(i)
-		if mnem == "call" and i != endEA:
-			print >>fp, "%#010x %#010x %s" % (first, idc.NextHead(i, endEA+1)-1, curName)
-			first=idc.NextHead(i, endEA+1)
-	
 
-	if first < endEA:
-		print >>fp, "%#010x %#010x %s" % (first, endEA-1, curName)		
-			
-# -----------------------------------------------------------------------
-# Using the class
-def cls_main(fp, func, p=True):
-    global f
-    f = idaapi.FlowChart(idaapi.get_func(func))
-    for block in f:
-		cls_split_block(fp, block.startEA, block.endEA)
-		#if p: print >>fp, "%#10x %#10x" % (block.startEA, block.endEA)
-        #for succ_block in block.succs():
-        #    if p: print "  %x - %x [%d]:" % (succ_block.startEA, succ_block.endEA, succ_block.id)
-        #for pred_block in block.preds():
-        #    if p: print "  %x - %x [%d]:" % (pred_block.startEA, pred_block.endEA, pred_block.id)
+def cls_split_block(fp, start_ea, end_ea):
+    """
+    ``idaapi.Flowchart`` does not consider function calls as basic block
+    boundaries. This function takes a range of addresses and splits additional
+    basic blocks.
+    """
+    cur_name = idc.GetFunctionName(start_ea)
+    dem_name = idc.Demangle(cur_name, idc.GetLongPrm(idc.INF_SHORT_DN))
+    if dem_name != None:
+        cur_name = dem_name
+
+    first = start_ea
+    block = idautils.Heads(start_ea, end_ea)
+    for inst in block:
+        mnem = idc.GetMnem(inst)
+        if mnem == 'call' and inst != end_ea:
+            fp.write('%#010x %#010x %s\n' % (first,
+                                             idc.NextHead(inst, end_ea + 1) - 1,
+                                             cur_name))
+            first = idc.NextHead(inst, end_ea + 1)
+
+    if first < end_ea:
+        fp.write('%#010x %#010x %s\n' % (first, end_ea - 1, cur_name))
+
+
+def cls_main(fp, func):
+    flow_chart = idaapi.FlowChart(idaapi.get_func(func))
+    for block in flow_chart:
+        cls_split_block(fp, block.startEA, block.endEA)
+
 
 def extract_bbs():
-	filename = idc.AskFile(1, "*.*", "Save list of basic blocks")
-        exit = False
-        if not filename:
-            basename = idc.GetInputFile()
-            filename = basename + ".bblist"
-            idc.GenerateFile(idc.OFILE_ASM, basename + ".asm", 0, idc.BADADDR, 0)
-            idc.GenerateFile(idc.OFILE_LST, basename + ".lst", 0, idc.BADADDR, 0)
-            exit = True
-	fp = open(filename,'w')
-	funcs = idautils.Functions()
-	for f in funcs:
-		cls_main(fp, f)
-        if exit:
-            idc.Exit(0)
-			
-q = None
-f = None
-idc.Wait()
-extract_bbs()
+    filename = idc.AskFile(1, '*.*', 'Save list of basic blocks')
+    do_exit = False
 
+    if not filename:
+        basename = idc.GetInputFile()
+        filename = '%s.bblist' % basename
+        idc.GenerateFile(idc.OFILE_ASM, '%s.asm' % basename, 0, idc.BADADDR, 0)
+        idc.GenerateFile(idc.OFILE_LST, '%s.lst' % basename, 0, idc.BADADDR, 0)
+        do_exit = True
+
+    with open(filename, 'w') as fp:
+        funcs = idautils.Functions()
+        for func in funcs:
+            cls_main(fp, func)
+
+    if do_exit:
+        idc.Exit(0)
+
+
+if __name__ == '__main__':
+    idc.Wait()
+    extract_bbs()
