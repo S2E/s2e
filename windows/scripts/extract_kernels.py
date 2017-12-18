d0a78f1785eb743ddc7ec620d21df55233a3912b
Vitaly Chipounov <vitaly@cyberhaven.io>
2017-12-18 01:01:07 +0100

    windows: refactored kernel extractions script
    - removed hard-coded image names
    - more modular
    
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- windows/scripts/extract_kernels.py
+++ windows/scripts/extract_kernels.py
@@ -36,6 +36,7 @@ from __future__ import print_function
 
 
 import argparse
+import glob
 import hashlib
 import os
 import re
@@ -53,33 +54,7 @@ import tempfile
 
 SEVEN_ZIP_VERSION_REGEX = re.compile(r'7-Zip .*?(?P<major>\d+)\.(?P<minor>\d+)')
 
-
-# Maps Windows ISOs (taken from the supported images listed in
-# s2e/guest-images/images.json) to the location of the kernel within the ISO.
-#
-# The kernel executable is typically stored within an intermediary container.
-# The path to this intermediary container is the mapped value in this
-# dictionary.
-KRNL_CONTAINER_MAP = {
-    'en_windows_xp_professional_with_service_pack_3_x86_cd_x14-80428.iso':
-        [os.path.join('I386', '*.EX_')],
-    'en_windows_7_enterprise_with_sp1_x64_dvd_u_677651.iso':
-        [os.path.join('sources', 'install.wim')],
-    'en_windows_8_1_enterprise_x64_dvd_2971902.iso':
-        [os.path.join('sources', 'install.wim')],
-    'en_windows_10_enterprise_version_1703_updated_march_2017_x64_dvd_10189290.iso':
-        [os.path.join('sources', 'install.wim')],
-}
-
-# We use the wildcard pattern "nt*.exe" to find kernel executables in a Windows
-# ISO. Unforuntaly, this can also match other executables that are not related
-# to the kernel. A blacklist of these executables is maintained here.
-NT_BLACKLIST = [
-    'ntprint.exe',
-    'ntbackup.exe',
-    'ntvdm.exe',
-]
-
+NT_PATTERN = re.compile('(ntoskrnl|ntkrnlmp|ntkrnlpa)$')
 
 # Adapted from http://stackoverflow.com/a/19299884/5894531
 class TemporaryDirectory(object):
@@ -124,7 +99,6 @@ class TemporaryDirectory(object):
             if warn:
                 print('Implicitly cleaning up {!r}'.format(self))
 
-
 def parse_args():
     """Parse the command-line arguments."""
     parser = argparse.ArgumentParser(description='Extract Windows kernels.')
@@ -152,16 +126,15 @@ def seven_zip_version():
 
     return int(m.group('major')), int(m.group('minor'))
 
-
 def seven_zip_extract(source, wildcard_includes, wildcard_excludes=None,
-                      dest=None):
+                      dest_dir=None):
     """
     Execute 7-Zip and extract the files from `source` that match the given
-    include wildcard patterns. A list of exclusion wldcard patterns may
+    include wildcard patterns. A list of exclusion wildcard patterns may
     optionally be given.
     """
-    if not dest:
-        dest = os.getcwd()
+    if not dest_dir:
+        dest_dir = os.getcwd()
 
     # Assemble the 7-Zip command-line arguments
     args = ['7z', 'e', '-aou']
@@ -177,23 +150,147 @@ def seven_zip_extract(source, wildcard_includes, wildcard_excludes=None,
 
     args.append(source)
 
+    print("    %s - %s" % (" ".join(args), dest_dir))
+
     # Run 7-Zip
-    proc = subprocess.Popen(args, stdout=DEVNULL, stderr=subprocess.PIPE,
-                            cwd=dest)
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=dest_dir)
     _, stderr = proc.communicate()
 
-    return proc.returncode, stderr
+    if proc.returncode:
+        raise Exception(u'Failed to extract file %s from %s: "%s"' % (wildcard_includes, source, stderr))
 
+def seven_zip_list(source):
+    args = ['7z', 'l', source]
+    proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+
+    files = []
+
+    for line in stdout.splitlines():
+        els = line.split()
+        if len(els) == 6:
+            files.append(els[5])
+
+    return files
 
 def calc_hash(path):
     """Calculate the hash of the file at the given location."""
     with open(path, 'r') as f:
         return hashlib.md5(f.read()).hexdigest()
 
-    # XXX Throw exception?
-    return ''
+def check_7z_version():
+    seven_zip_ver = seven_zip_version()
+    if not seven_zip_ver:
+        print(u'[\u2717] Unable to determine 7-Zip version. Some kernels may '
+              u'not be extracted')
+    elif (seven_zip_ver[0] < 9 or
+              (seven_zip_ver[0] == 9 and seven_zip_ver[1] < 30)):
+        print(u'[\u2717] This version of 7-Zip (%d.%d) is unable to extract '
+              u'WIM images from Windows 8 or newer - no kernels will be '
+              u'extracted from those ISOs.\n    7-Zip 9.30 or newer is '
+              u'required to extract files from Windows 8 WIM '
+              u'images' % (seven_zip_ver[0], seven_zip_ver[1]))
 
+def get_full_name(container_path, filepath):
+    hash = calc_hash(filepath)
+    basename = os.path.basename(filepath)
+    container, _ = os.path.splitext(os.path.basename(container_path))
 
+    return '%s_%s_%s' % (container, hash, basename)
+
+def get_base_name(path):
+    filename = os.path.basename(path)
+    base, _ = os.path.splitext(filename)
+    return base
+
+def filter_kernels(files):
+    ret = []
+
+    for file in files:
+        base = get_base_name(file.lower())
+
+        if NT_PATTERN.match(base):
+           ret.append(file)
+
+    return ret
+
+def extract_file(output_dir, container, source):
+    source_name = os.path.basename(source)
+
+    with TemporaryDirectory() as temp_dir:
+        seven_zip_extract(container, [source], dest_dir=temp_dir)
+        return expand_file(output_dir, os.path.join(temp_dir, source_name))
+
+def expand_file(output_dir, filepath):
+    filename = os.path.basename(filepath).lower()
+    if not filename.endswith('.ex_'):
+        dest_path = os.path.join(output_dir, filename)
+        os.rename(filepath, dest_path)
+        return dest_path
+
+    dest_path = os.path.join(output_dir, filename.replace('.ex_', '.exe'))
+    with TemporaryDirectory() as temp_dir:
+        seven_zip_extract(filepath, [], dest_dir=temp_dir)
+        file = glob.glob(os.path.join(temp_dir, '*.exe'))[0]
+        os.rename(file, dest_path)
+
+    return dest_path
+
+def is_valid_kernel(path):
+    with open(path, 'rb') as fp:
+        b = fp.read(2)
+        return b == 'MZ'
+
+def extract_kernels_from_container(output_dir, container, files):
+    kernels = filter_kernels(files)
+    for kernel in kernels:
+        dest_path = extract_file(output_dir, container, kernel)
+        new_name = get_full_name(container, dest_path)
+        final_path = os.path.join(output_dir, new_name)
+        os.rename(dest_path, final_path)
+        if not is_valid_kernel(final_path):
+            print(u'    [\u2717] %s is not a valid kernel' % final_path)
+        else:
+            print(u'    [\u2713] Extracted %s' % final_path)
+
+def extract_kernels_wim(output_dir, iso_file):
+    iso_basename, _ = os.path.splitext(os.path.basename(iso_file))
+
+    with TemporaryDirectory() as temp_dir:
+        seven_zip_extract(iso_file, ['sources/install.wim'], dest_dir=temp_dir)
+        wim = os.path.join(temp_dir, 'install.wim')
+
+        # Rename the wim to have more informative kernel name
+        dest_wim = os.path.join(temp_dir, iso_basename + '.wim')
+        os.rename(wim, dest_wim)
+
+        files = seven_zip_list(dest_wim)
+        extract_kernels_from_container(output_dir, dest_wim, files)
+
+def extract_kernels_from_iso(output_dir, iso_file):
+    # First, guess the OS version we have.
+    # Windows XP has an I386 folder, while later ones contain install.wim
+    files = seven_zip_list(iso_file)
+
+    if 'sources/install.wim' in files:
+        # Extract the wim, then extract files from that wim
+        extract_kernels_wim(output_dir, iso_file)
+    elif 'WIN51' in files:
+        extract_kernels_from_container(output_dir, iso_file, files)
+    else:
+        print("Invalid install iso")
+
+def extract_kernels(output_dir, filepath):
+    print('Extracting kernels from %s' % filepath)
+
+    _, ext = os.path.splitext(filepath)
+    if ext == '.iso':
+        extract_kernels_from_iso(output_dir, filepath)
+    elif ext == '.wim':
+        files = seven_zip_list(filepath)
+        extract_kernels_from_container(output_dir, filepath, files)
+    else:
+        print('Unknown file type: %s' % ext)
 
 def main():
     """The main function."""
@@ -211,84 +308,12 @@ def main():
     output_dir = os.path.realpath(output_dir)
 
     # Check the version of 7-Zip available
-    seven_zip_ver = seven_zip_version()
-    if not seven_zip_ver:
-        print(u'[\u2717] Unable to determine 7-Zip version. Some kernels may '
-              u'not be extracted')
-    elif(seven_zip_ver[0] < 9 or
-         (seven_zip_ver[0] == 9 and seven_zip_ver[1] < 30)):
-        print(u'[\u2717] This version of 7-Zip (%d.%d) is unable to extract '
-              u'WIM images from Windows 8 or newer - no kernels will be '
-              u'extracted from those ISOs.\n    7-Zip 9.30 or newer is '
-              u'required to extract files from Windows 8 WIM '
-              u'images' % (seven_zip_ver[0], seven_zip_ver[1]))
+    check_7z_version()
 
-    # Maintain a set of hashes for kernels that we've already extraced. This is
-    # to prevent extracting the same kernel multiple times
-    krnl_hashes = set()
-
-    for iso, containers in KRNL_CONTAINER_MAP.items():
-        iso_path = os.path.join(iso_dir, iso)
-        if not os.path.isfile(iso_path):
-            print(u'[\u2717] %s does not exist. Skipping...' % iso)
-            continue
-
-        iso_name, _ = os.path.splitext(iso)
-
-        with TemporaryDirectory() as temp_dir:
-            print('[-] Extracting kernels from %s...' % iso)
-
-            for container in containers:
-                print('[-]   Looking for kernels in %s...' % container)
-
-                # Extract the kernel container
-                returncode, stderr = seven_zip_extract(iso_path, [container],
-                                                       dest=temp_dir)
-                if returncode:
-                    print(u'[\u2717]   Failed to extract %s from %s: "%s"' %
-                          (container, iso, stderr))
-                    continue
-
-                container_name = os.path.basename(container)
-                container_path = os.path.join(temp_dir, container_name)
-
-                # Extract the kernel executable(s)
-                returncode, stderr = seven_zip_extract(container_path,
-                                                       ['nt*.exe'],
-                                                       NT_BLACKLIST, temp_dir)
-                if returncode:
-                    print(u'[\u2717]     Failed to extract kernels from %s: '
-                          u'"%s"' % (container, stderr))
-                    continue
-
-                # Copy the extracted kernels from the temporary directory to
-                # the given output directory
-                for root, _, files in os.walk(temp_dir):
-                    for file_ in files:
-                        # The kernel will be an executable file
-                        _, file_ext = os.path.splitext(file_)
-                        if file_ext != '.exe':
-                            continue
-
-                        # The output file is named after the ISO, hash of the
-                        # file and the original file name
-                        krnl_path = os.path.join(root, file_)
-                        krnl_hash = calc_hash(krnl_path)
-
-                        # If we've already saved this kernel, ignore it
-                        if krnl_hash in krnl_hashes:
-                            continue
-
-                        output_file = '%s_%s_%s' % (iso_name, krnl_hash, file_)
-                        output_path = os.path.join(output_dir, output_file)
-
-                        shutil.move(krnl_path, output_path)
-
-                        krnl_hashes.add(krnl_hash)
-
-                        print(u'[\u2713]     Successfully extracted %s to %s' %
-                              (file_, output_file))
-
+    for root, _, files in os.walk(iso_dir):
+        for f in files:
+            filepath = os.path.join(root, f)
+            extract_kernels(output_dir, filepath)
 
 if __name__ == '__main__':
     main()
