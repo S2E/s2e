a96ed0d22e397438aa93c52a64c243c86c14615b
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-01-06 19:28:19 +0100

    windows/driver: cleaned up kernel function lookup
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- windows/driver/src/kernel_functions.c
+++ windows/driver/src/kernel_functions.c
@@ -8,6 +8,7 @@
 
 #include <ntddk.h>
 #include <s2e/s2e.h>
+#include "log.h"
 #include "kernel_functions.h"
 
 PSGETPROCESSPB g_pPsGetProcessPeb;
@@ -16,21 +17,31 @@ GET_PROCESS_IMAGE_NAME g_pGetProcessImageFileName;
 // MmGetSystemRoutineAddress returns PVOID when it shoudl return FARPROC
 #pragma warning(disable:4055)
 
-VOID InitializeKernelFunctionPointers(VOID)
+NTSTATUS InitializeKernelFunctionPointers(VOID)
 {
+    NTSTATUS Status;
     UNICODE_STRING MethodName;
 
     RtlInitUnicodeString(&MethodName, L"PsGetProcessPeb");
     g_pPsGetProcessPeb = (PSGETPROCESSPB)MmGetSystemRoutineAddress(&MethodName);
 
     if (!g_pPsGetProcessPeb) {
-        S2EKillState(0, "could not find PsGetProcessPeb routine\n");
+        LOG("Could not find PsGetProcessPeb routine\n");
+        Status = STATUS_NOT_FOUND;
+        goto err;
     }
 
     RtlInitUnicodeString(&MethodName, L"PsGetProcessImageFileName");
     g_pGetProcessImageFileName = (GET_PROCESS_IMAGE_NAME)MmGetSystemRoutineAddress(&MethodName);
 
     if (!g_pGetProcessImageFileName) {
-        S2EKillState(0, "could not find PsGetProcessImageFileName routine\n");
+        LOG("Could not find PsGetProcessImageFileName routine\n");
+        Status = STATUS_NOT_FOUND;
+        goto err;
     }
+
+    Status = STATUS_SUCCESS;
+
+err:
+    return Status;
 }
