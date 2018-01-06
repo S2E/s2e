a6a9a1d0e3b20cda822d4e57c85cc1747bfed496
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-01-06 19:18:18 +0100

    windows/driver: added string apis
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

---
+++ windows/driver/src/adt/strings.c
@@ -0,0 +1,63 @@
+#include <ntddk.h>
+#include <ntstrsafe.h>
+#include "../log.h"
+#include "strings.h"
+
+#define TAG_STR 'stri'
+
+NTSTATUS StringToUnicode(_In_ LPCSTR String, _In_ SIZE_T MaxInputLen, _Out_ PUNICODE_STRING Unicode)
+{
+    NTSTATUS Status;
+    SIZE_T Len;
+    PVOID Buffer;
+
+    Status = RtlStringCchLengthA(String, MaxInputLen, &Len);
+    if (!NT_SUCCESS(Status)) {
+        LOG("Could not get string length\n");
+        goto err;
+    }
+
+    Buffer = ExAllocatePoolWithTag(NonPagedPool, MaxInputLen * sizeof(WCHAR), TAG_STR);
+    if (!Buffer) {
+        LOG("Could not allocate buffer for string\n");
+        Status = STATUS_INSUFFICIENT_RESOURCES;
+        goto err;
+    }
+
+    Unicode->Buffer = Buffer;
+    Unicode->MaximumLength = (USHORT)MaxInputLen;
+    Unicode->Length = (USHORT)Len;
+
+    for (unsigned i = 0; i < Unicode->Length; ++i) {
+        Unicode->Buffer[i] = String[i];
+    }
+
+    Status = STATUS_SUCCESS;
+
+err:
+    return Status;
+}
+
+VOID StringFree(_Inout_ PUNICODE_STRING String)
+{
+    if (String->Buffer) {
+        ExFreePoolWithTag(String->Buffer, TAG_STR);
+    }
+
+    String->Buffer = NULL;
+    String->MaximumLength = 0;
+    String->Length = 0;
+}
+
+NTSTATUS StringDuplicate(_Out_ PUNICODE_STRING Dest, _In_ PCUNICODE_STRING Source)
+{
+    *Dest = *Source;
+    Dest->Buffer = (PWCH)ExAllocatePoolWithTag(NonPagedPool, Source->MaximumLength, 0xdead);
+    if (!Dest->Buffer) {
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    memcpy(Dest->Buffer, Source->Buffer, Source->MaximumLength);
+
+    return STATUS_SUCCESS;
+}
