647bf49707c54963bc58f2dbf0b3e331de412efc
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-01-13 14:38:33 +0100

    windows/driver: added backtrace printing api
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

---
+++ windows/driver/src/utils.h
@@ -0,0 +1,17 @@
+///
+/// Copyright (C) 2014-2016, Dependable Systems Laboratory, EPFL
+/// Copyright (C) 2014-2017, Cyberhaven
+/// All rights reserved.
+///
+/// Licensed under the Cyberhaven Research License Agreement.
+///
+
+#pragma once
+
+VOID S2EDumpBackTrace(VOID);
+
+NTSTATUS S2EEncodeBackTraceForKnownModules(
+    _Out_ PCHAR *Buffer,
+    _Out_opt_ PULONG Hash,
+    _In_ ULONG FramesToSkip
+);
