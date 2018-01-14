ec025553287c6af33b91212214bf99e6cf92d23c
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-01-14 21:38:45 +0100

    windows: fixed makedist to copy master inf file
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- windows/makedist.bat
+++ windows/makedist.bat
@@ -2,7 +2,7 @@
 :: The resulting files may be uploaded to the github release page.
 
 md dist
-copy Release\s2e.inf dist
+copy driver\s2e.inf dist
 copy Release\s2e.sys dist\s2e32.sys
 copy x64\Release\s2e.sys dist\s2e.sys
 copy Release\drvctl.exe dist\drvctl32.exe
