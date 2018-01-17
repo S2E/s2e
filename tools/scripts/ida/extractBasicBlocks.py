c7f3055c3d9ffa8febe3846563cd22e8e3e519b6
Adrian Herrera <adrian.herrera02@gmail.com>
2018-01-18 10:49:53 +1100

    extractBasicBlocks: updated inline with s2e-env changes
    s2e-env basic block coverage analysis now caches disassembly information to
    disk as a JSON file. The extractBasicBlocks.py script has been updated to do
    the same.

--- tools/scripts/ida/extractBasicBlocks.py
+++ tools/scripts/ida/extractBasicBlocks.py
@@ -1,54 +1,88 @@
+import json
+
 import idaapi
 import idautils
 import idc
 
 
-def cls_split_block(fp, start_ea, end_ea):
+def _split_basic_block(start_ea, end_ea):
     """
-    ``idaapi.Flowchart`` does not consider function calls as basic block
-    boundaries. This function takes a range of addresses and splits additional
-    basic blocks.
+    IDA Pro's ``idaapi.Flowchart`` does not consider function calls as basic
+    block boundaries. This function takes an address range and splits the
+    basic blocks found within that range so that function calls are considered
+    basic block boundaries.
     """
-    cur_name = idc.GetFunctionName(start_ea)
-    dem_name = idc.Demangle(cur_name, idc.GetLongPrm(idc.INF_SHORT_DN))
-    if dem_name != None:
-        cur_name = dem_name
+    split_bbs = []
+    func_name = idc.GetFunctionName(start_ea)
+    demangled_name = idc.Demangle(func_name,
+                                  idc.GetLongPrm(idc.INF_SHORT_DN))
+    if demangled_name:
+        func_name = demangled_name
 
-    first = start_ea
+    bb_start_addr = start_ea
     block = idautils.Heads(start_ea, end_ea)
+
     for inst in block:
         mnem = idc.GetMnem(inst)
         if mnem == 'call' and inst != end_ea:
-            fp.write('%#010x %#010x %s\n' % (first,
-                                             idc.NextHead(inst, end_ea + 1) - 1,
-                                             cur_name))
-            first = idc.NextHead(inst, end_ea + 1)
+            split_bbs.append(dict(start_addr=bb_start_addr,
+                                  end_addr=idc.NextHead(inst, end_ea + 1) - 1,
+                                  function=func_name))
+            bb_start_addr = idc.NextHead(inst, end_ea + 1)
 
-    if first < end_ea:
-        fp.write('%#010x %#010x %s\n' % (first, end_ea - 1, cur_name))
+    if bb_start_addr < end_ea:
+        split_bbs.append(dict(start_addr=bb_start_addr, end_addr=end_ea - 1,
+                              function=func_name))
 
-
-def cls_main(fp, func):
-    flow_chart = idaapi.FlowChart(idaapi.get_func(func))
-    for block in flow_chart:
-        cls_split_block(fp, block.startEA, block.endEA)
+    return split_bbs
 
 
-def extract_bbs():
-    filename = idc.AskFile(1, '*.*', 'Save list of basic blocks')
+def _get_basic_blocks():
+    """
+    Extract basic block information from the target binary.
+    """
+    bbs = []
+
+    for func in idautils.Functions():
+        flowchart = idaapi.FlowChart(idaapi.get_func(func))
+
+        for basic_block in flowchart:
+            split_bbs = _split_basic_block(basic_block.startEA,
+                                           basic_block.endEA)
+            bbs.extend(split_bbs)
+
+    return bbs
+
+
+def main():
+    disas_path = idc.AskFile(1, '*.disas', 'Save basic blocks')
     do_exit = False
 
-    if not filename:
+    if not disas_path:
         basename = idc.GetInputFile()
-        filename = '%s.bblist' % basename
+        disas_path = '%s.disas' % basename
         idc.GenerateFile(idc.OFILE_ASM, '%s.asm' % basename, 0, idc.BADADDR, 0)
         idc.GenerateFile(idc.OFILE_LST, '%s.lst' % basename, 0, idc.BADADDR, 0)
         do_exit = True
 
-    with open(filename, 'w') as fp:
-        funcs = idautils.Functions()
-        for func in funcs:
-            cls_main(fp, func)
+    # Get basic blocks
+    bbs = _get_basic_blocks()
+
+    # Get the module's base address
+    base_addr = idaapi.get_imagebase()
+
+    # Get the module's end address
+    segs = sorted(idautils.Segments())
+    end_addr = idc.SegEnd(segs[-1])
+
+    disas_info = {
+        'bbs': bbs,
+        'base_addr': base_addr,
+        'end_addr': end_addr,
+    }
+
+    with open(disas_path, 'w') as disas_file:
+        json.dump(disas_info, disas_file)
 
     if do_exit:
         idc.Exit(0)
@@ -56,4 +90,4 @@ def extract_bbs():
 
 if __name__ == '__main__':
     idc.Wait()
-    extract_bbs()
+    main()
