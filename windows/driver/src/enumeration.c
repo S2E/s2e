a1a42be838abfaa90de9886118457a7f8fa34f50
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-02-11 22:05:03 +0100

    windows/driver: pass full process name to windows monitor
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- windows/driver/src/enumeration.c
+++ windows/driver/src/enumeration.c
@@ -6,18 +6,21 @@
 /// Licensed under the Cyberhaven Research License Agreement.
 ///
 
-#include <s2e/s2e.h>
 #include <ntddk.h>
+
+#include <s2e/s2e.h>
+#include "adt/strings.h"
 #include "kernel_structs.h"
 #include "kernel_functions.h"
+#include "utils/process.h"
 #include "log.h"
 
 VOID EnumerateThreads(PEPROCESS Process)
 {
-    PLIST_ENTRY Head = (PLIST_ENTRY)((UINT_PTR)(Process)+(UINT_PTR)g_kernelStructs.EProcessThreadListHeadOffset);
+    PLIST_ENTRY Head = (PLIST_ENTRY)((UINT_PTR)(Process) + (UINT_PTR)g_kernelStructs.EProcessThreadListHeadOffset);
     PLIST_ENTRY CurrentThreadLink = Head->Flink;
     while (CurrentThreadLink != Head) {
-        UINT_PTR pEThread = (UINT_PTR)(CurrentThreadLink)-(UINT_PTR)g_kernelStructs.EThreadThreadListEntry;
+        UINT_PTR pEThread = (UINT_PTR)(CurrentThreadLink) - (UINT_PTR)g_kernelStructs.EThreadThreadListEntry;
         LOG("   ETHREAD %#p ID=%#x\n", pEThread, PsGetThreadId((PETHREAD)pEThread));
         CurrentThreadLink = CurrentThreadLink->Flink;
     }
@@ -31,12 +34,23 @@ VOID EnumerateProcesses(VOID)
     PLIST_ENTRY CurrentProcessLink = Head->Flink;
 
     while (CurrentProcessLink != Head) {
-        CHAR *ImageFileName;
+        UNICODE_STRING ProcessImageName = { 0 };
+        NTSTATUS Status;
 
-        UINT_PTR pEProcess = (UINT_PTR)(CurrentProcessLink)-(UINT_PTR)g_kernelStructs.EProcessActiveProcessLinkOffset;
-        ImageFileName = g_pGetProcessImageFileName((PEPROCESS)pEProcess);
-        LOG("EPROCESS %#p ID=%#x %s\n", pEProcess, PsGetProcessId((PEPROCESS)pEProcess), ImageFileName);
-        EnumerateThreads((PEPROCESS)pEProcess);
+        UINT_PTR EProcess = (UINT_PTR)(CurrentProcessLink) - (UINT_PTR)g_kernelStructs.EProcessActiveProcessLinkOffset;
+
+        Status = ProcessGetImageName((PEPROCESS)EProcess, &ProcessImageName);
+        if (!Status) {
+            LOG("Could not get image name for process\n");
+            goto err;
+        }
+
+        LOG("EPROCESS %#p ID=%#x %wZ\n", EProcess, PsGetProcessId((PEPROCESS)EProcess), &ProcessImageName);
+        EnumerateThreads((PEPROCESS)EProcess);
+
+    err:
+        StringFree(&ProcessImageName);
+
         CurrentProcessLink = CurrentProcessLink->Flink;
     }
 }
