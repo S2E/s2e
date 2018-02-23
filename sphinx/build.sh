dc1e122c1aa79e007b4f9cba41a6e963c726b341
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-02-23 15:23:02 +0100

    sphinx: added google analytics
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- sphinx/build.sh
+++ sphinx/build.sh
@@ -4,6 +4,8 @@
 # The original source should be kept as much as possible in Github's dialect so that people
 # may use Github to browse the documentation.
 
+# This is optional
+GOOGLE_ANALYTICS_ID="$1"
 
 # Copy files into a temp folder, then patch them to make sphinx happy
 mkdir -p source1
@@ -19,3 +21,8 @@ make html
 
 echo "Replacing .rst with .html in links"
 $(cd build/html && sed -i 's/\.rst\"/\.html\"/g' $(find . -name '*.html'))
+
+if [ "x${GOOGLE_ANALYTICS_ID}" != "x" ]; then
+  echo "Customizing google analytics id"
+  $(cd build/html && sed -i "s/UA-XXXX-X/${GOOGLE_ANALYTICS_ID}/g" $(find . -name '*.html'))
+fi
