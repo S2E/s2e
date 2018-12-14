adf6b8198633640472071c08241e1b19db49fe32
Vitaly Chipounov <vitaly@cyberhaven.io>
2018-12-15 00:21:26 +0100

    sphinx: tweaked options
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -113,7 +113,21 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'canonical_url': '',
+    'logo_only': False,
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
