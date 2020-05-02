77bc7139b346e3aa0752bd816526f9a052eeea70
Vitaly Chipounov <vitaly@cyberhaven.io>
2020-05-02 21:14:41 +0200

    klee/BitArray: switched to 64-bit size + clean ups
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- klee/include/klee/util/BitArray.h
+++ klee/include/klee/util/BitArray.h
@@ -18,15 +18,11 @@
 
 namespace klee {
 
-class BitArray;
-typedef boost::intrusive_ptr<BitArray> BitArrayPtr;
-
-class BitArray {
+template <typename T> class BitArrayT {
 private:
     // XXX(s2e) for now we keep this first to access from C code
     // (yes, we do need to access if really fast)
-    // TODO: make this uint64_t
-    uint32_t *m_bits;
+    T *m_bits;
     std::atomic<unsigned> m_refCount;
     unsigned m_bitcount;
     unsigned m_setbitcount;
@@ -35,53 +31,34 @@ private:
     static const auto BITSM1 = BITS - 1;
 
 protected:
-    static uint32_t length(unsigned _size) {
-        return (_size + BITSM1) / BITS;
+    static unsigned words(unsigned _bitcount) {
+        return (_bitcount + BITSM1) / BITS;
     }
 
-    unsigned popcount_4(uint32_t x) const {
-        int count;
-        for (count = 0; x; count++)
-            x &= x - 1;
-        return count;
+    BitArrayT(unsigned size, bool value = false)
+        : m_bits(new T[words(size)]), m_refCount(0), m_bitcount(size), m_setbitcount(value ? size : 0) {
+        memset(m_bits, value ? 0xFF : 0, sizeof(*m_bits) * words(size));
     }
 
-    unsigned computePopCount() const {
-        unsigned acc = 0;
-        unsigned len = m_bitcount / BITS;
-        for (unsigned i = 0; i < len; ++i) {
-            acc += popcount_4(m_bits[i]);
-        }
-        if (m_bitcount % BITS) {
-            uint32_t mask = (1 << (m_bitcount & (BITS - 1))) - 1;
-            acc += popcount_4(m_bits[len] & mask);
-        }
-        return acc;
-    }
-
-    BitArray(unsigned size, bool value = false)
-        : m_bits(new uint32_t[length(size)]), m_refCount(0), m_bitcount(size), m_setbitcount(value ? size : 0) {
-        memset(m_bits, value ? 0xFF : 0, sizeof(*m_bits) * length(size));
-    }
-
-    BitArray(const BitArrayPtr &b) : m_bits(nullptr), m_refCount(0), m_bitcount(0), m_setbitcount(0) {
+    BitArrayT(const boost::intrusive_ptr<BitArrayT> &b)
+        : m_bits(nullptr), m_refCount(0), m_bitcount(0), m_setbitcount(0) {
         m_bitcount = b->m_bitcount;
         m_setbitcount = b->m_setbitcount;
-        m_bits = new uint32_t[length(m_bitcount)];
-        memcpy(m_bits, b->m_bits, sizeof(*m_bits) * length(m_bitcount));
+        m_bits = new T[words(m_bitcount)];
+        memcpy(m_bits, b->m_bits, sizeof(*m_bits) * words(m_bitcount));
+    }
+
+    ~BitArrayT() {
+        delete[] m_bits;
     }
 
 public:
-    static BitArrayPtr create(unsigned size, bool value = false) {
-        return BitArrayPtr(new BitArray(size, value));
+    static boost::intrusive_ptr<BitArrayT> create(unsigned size, bool value = false) {
+        return boost::intrusive_ptr<BitArrayT>(new BitArrayT(size, value));
     }
 
-    static BitArrayPtr create(const BitArrayPtr &b) {
-        return BitArrayPtr(new BitArray(b));
-    }
-
-    ~BitArray() {
-        delete[] m_bits;
+    static boost::intrusive_ptr<BitArrayT> create(const boost::intrusive_ptr<BitArrayT> &b) {
+        return boost::intrusive_ptr<BitArrayT>(new BitArrayT(b));
     }
 
     inline unsigned getBitCount() const {
@@ -89,21 +66,21 @@ public:
     }
 
     inline bool get(unsigned idx) const {
-        return (bool) ((m_bits[idx / BITS] >> (idx & BITSM1)) & 1);
+        return (bool) ((m_bits[idx / BITS] >> (T)(idx & BITSM1)) & 1);
     }
 
     inline void set(unsigned idx) {
         if (!get(idx)) {
             ++m_setbitcount;
         }
-        m_bits[idx / BITS] |= 1 << (idx & BITSM1);
+        m_bits[idx / BITS] |= (T) 1 << (T)(idx & BITSM1);
     }
 
     inline void unset(unsigned idx) {
         if (get(idx)) {
             --m_setbitcount;
         }
-        m_bits[idx / BITS] &= ~(1 << (idx & BITSM1));
+        m_bits[idx / BITS] &= ~((T) 1 << (T)(idx & BITSM1));
     }
 
     inline void set(unsigned idx, bool value) {
@@ -134,13 +111,31 @@ public:
         return m_setbitcount;
     }
 
+    static inline int ctz64(uint64_t val) {
+        return val ? __builtin_ctzll(val) : 64;
+    }
+
+    static inline int ctz32(uint32_t val) {
+        return val ? __builtin_ctz(val) : 32;
+    }
+
+    static inline int ctz(T val) {
+        if (sizeof(T) == sizeof(uint32_t)) {
+            return ctz32(val);
+        } else if (sizeof(T) == sizeof(uint64_t)) {
+            return ctz64(val);
+        } else {
+            abort();
+        }
+    }
+
     bool findFirstSet(unsigned &index) const {
-        auto words = length(m_bitcount);
-        for (auto i = 0u; i < words; ++i) {
+        auto nwords = words(m_bitcount);
+        for (auto i = 0u; i < nwords; ++i) {
             if (!m_bits[i]) {
                 continue;
             } else {
-                auto firstBitSet = ffs(m_bits[i]);
+                auto firstBitSet = ctz(m_bits[i]) + 1;
                 assert(firstBitSet);
                 index = i * BITS + firstBitSet - 1;
                 assert(get(index));
@@ -151,10 +146,14 @@ public:
         return false;
     }
 
-    INTRUSIVE_PTR_FRIENDS(BitArray)
+    INTRUSIVE_PTR_FRIENDS(BitArrayT)
 };
 
-INTRUSIVE_PTR_ADD_REL(BitArray)
+using BitArray = BitArrayT<uint64_t>;
+using BitArrayPtr = boost::intrusive_ptr<BitArray>;
+
+INTRUSIVE_PTR_ADD_REL(BitArrayT<uint64_t>)
+INTRUSIVE_PTR_ADD_REL(BitArrayT<uint32_t>)
 
 } // namespace klee
 
