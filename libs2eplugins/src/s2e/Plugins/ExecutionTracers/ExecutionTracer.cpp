318b0d6ee65b90593733a6fe7c4c9054b367a066
Vitaly Chipounov <vitaly@cyberhaven.com>
2021-02-08 23:49:17 +0100

    ExecutionTracer: record thread id
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.com>

--- libs2eplugins/src/s2e/Plugins/ExecutionTracers/ExecutionTracer.cpp
+++ libs2eplugins/src/s2e/Plugins/ExecutionTracers/ExecutionTracer.cpp
@@ -139,8 +139,10 @@ uint32_t ExecutionTracer::writeData(S2EExecutionState *state, const void *data, 
 
     if (m_monitor && m_monitor->initialized()) {
         header.set_pid(m_monitor->getPid(state));
+        header.set_tid(m_monitor->getTid(state));
     } else {
         header.set_pid(0);
+        header.set_tid(0);
     }
 
     // We must take the guid instead of the id, because duplicate ids
