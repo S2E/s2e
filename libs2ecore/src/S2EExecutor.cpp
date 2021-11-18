d9161bbd0970a52cdfda2dcc7af8f17d75972eb1
Vitaly Chipounov <vitaly@cyberhaven.com>
2021-11-18 22:12:50 +0100

    s2e/executor: fixed crash caused by uninitialized vector
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.com>

--- libs2ecore/src/S2EExecutor.cpp
+++ libs2ecore/src/S2EExecutor.cpp
@@ -1419,8 +1419,8 @@ S2EExecutor::StatePair S2EExecutor::doFork(ExecutionState &current, const klee::
         return res;
     }
 
-    llvm::SmallVector<S2EExecutionState *, 2> newStates;
-    llvm::SmallVector<klee::ref<Expr>, 2> newConditions;
+    llvm::SmallVector<S2EExecutionState *, 2> newStates(2);
+    llvm::SmallVector<klee::ref<Expr>, 2> newConditions(2);
 
     newStates[0] = static_cast<S2EExecutionState *>(res.first);
     newStates[1] = static_cast<S2EExecutionState *>(res.second);
