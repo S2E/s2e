b025c64a89fc3fd98bc8c6fd95c33a352a23b02d
Vitaly Chipounov <vitaly@cyberhaven.com>
2022-04-17 01:06:37 +0200

    scripts: added support for debian 11 and ubuntu 22.04
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.com>

--- scripts/determine_clang_binary_suffix.py
+++ scripts/determine_clang_binary_suffix.py
@@ -51,7 +51,7 @@ def _get_debian_version(version_string):
     version = int(version_string)
 
     if version >= 8:
-        return 'x86_64-linux-gnu-debian8'
+        return 'x86_64-linux-gnu-ubuntu-18.04'
     else:
         return None
 
@@ -73,13 +73,15 @@ def _get_ubuntu_version(version_string):
         return 'x86_64-linux-gnu-ubuntu-18.04',
     elif major_version == 20:
         return 'x86_64-linux-gnu-ubuntu-18.04',
+    elif major_version == 22:
+        return 'x86_64-linux-gnu-ubuntu-18.04',
     else:
         return None
 
 
 def main():
     """The main function."""
-    name = distro.name()
+    name = distro.id()
     version = distro.version()
 
     clang_ver_to_download = None
