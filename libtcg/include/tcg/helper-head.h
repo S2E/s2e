a60ab4727cc852d5a5ac616de648e314c8a9ae8c
Vitaly Chipounov <vitaly@chipounov.fr>
2023-05-29 13:26:23 +0200

    libtcg/include: updated header macros
    Signed-off-by: Vitaly Chipounov <vitaly@chipounov.fr>

--- libtcg/include/tcg/helper-head.h
+++ libtcg/include/tcg/helper-head.h
@@ -46,20 +46,23 @@
 #define dh_ctype_ptr      void *
 #define dh_ctype_cptr     const void *
 #define dh_ctype_void     void
-#define dh_ctype_noreturn void QEMU_NORETURN
+#define dh_ctype_noreturn G_NORETURN void
 #define dh_ctype(t)       dh_ctype_##t
 
 //#ifdef NEED_CPU_H
 #ifdef TARGET_LONG_BITS
 #if TARGET_LONG_BITS == 32
-#define dh_alias_tl i32
+#define dh_alias_tl    i32
+#define dh_typecode_tl dh_typecode_i32
 #else
-#define dh_alias_tl i64
+#define dh_alias_tl    i64
+#define dh_typecode_tl dh_typecode_i64
 #endif
 #endif
-#define dh_alias_env ptr
-#define dh_ctype_tl  target_ulong
-#define dh_ctype_env CPUArchState *
+#define dh_ctype_tl     target_ulong
+#define dh_alias_env    ptr
+#define dh_ctype_env    CPUArchState *
+#define dh_typecode_env dh_typecode_ptr
 //#endif
 
 /* We can't use glue() here because it falls foul of C preprocessor
@@ -85,60 +88,41 @@
 #define dh_retvar_ptr      tcgv_ptr_temp(retval)
 #define dh_retvar(t)       glue(dh_retvar_, dh_alias(t))
 
-#define dh_is_64bit_void     0
-#define dh_is_64bit_noreturn 0
-#define dh_is_64bit_i32      0
-#define dh_is_64bit_i64      1
-#define dh_is_64bit_ptr      (sizeof(void *) == 8)
-#define dh_is_64bit_cptr     dh_is_64bit_ptr
-#define dh_is_64bit(t)       glue(dh_is_64bit_, dh_alias(t))
-
-#define dh_is_signed_void     0
-#define dh_is_signed_noreturn 0
-#define dh_is_signed_i32      0
-#define dh_is_signed_s32      1
-#define dh_is_signed_i64      0
-#define dh_is_signed_s64      1
-#define dh_is_signed_f16      0
-#define dh_is_signed_f32      0
-#define dh_is_signed_f64      0
-#define dh_is_signed_tl       0
-#define dh_is_signed_int      1
-/* ??? This is highly specific to the host cpu.  There are even special
-   extension instructions that may be required, e.g. ia64's addp4.  But
-   for now we don't support any 64-bit targets with 32-bit pointers.  */
-#define dh_is_signed_ptr  0
-#define dh_is_signed_cptr dh_is_signed_ptr
-#define dh_is_signed_env  dh_is_signed_ptr
-#define dh_is_signed(t)   dh_is_signed_##t
+#define dh_typecode_void     0
+#define dh_typecode_noreturn 0
+#define dh_typecode_i32      2
+#define dh_typecode_s32      3
+#define dh_typecode_i64      4
+#define dh_typecode_s64      5
+#define dh_typecode_ptr      6
+#define dh_typecode_int      dh_typecode_s32
+#define dh_typecode_f16      dh_typecode_i32
+#define dh_typecode_f32      dh_typecode_i32
+#define dh_typecode_f64      dh_typecode_i64
+#define dh_typecode_cptr     dh_typecode_ptr
+#define dh_typecode(t)       dh_typecode_##t
 
 #define dh_callflag_i32      0
-#define dh_callflag_s32      0
-#define dh_callflag_int      0
 #define dh_callflag_i64      0
-#define dh_callflag_s64      0
-#define dh_callflag_f16      0
-#define dh_callflag_f32      0
-#define dh_callflag_f64      0
 #define dh_callflag_ptr      0
-#define dh_callflag_cptr     dh_callflag_ptr
 #define dh_callflag_void     0
 #define dh_callflag_noreturn TCG_CALL_NO_RETURN
 #define dh_callflag(t)       glue(dh_callflag_, dh_alias(t))
 
-#define dh_sizemask(t, n) ((dh_is_64bit(t) << (n * 2)) | (dh_is_signed(t) << (n * 2 + 1)))
+#define dh_typemask(t, n) (dh_typecode(t) << (n * 3))
 
 #define dh_arg(t, n) glue(glue(tcgv_, dh_alias(t)), _temp)(glue(arg, n))
 
 #define dh_arg_decl(t, n) glue(TCGv_, dh_alias(t)) glue(arg, n)
 
-#define DEF_HELPER_0(name, ret)                         DEF_HELPER_FLAGS_0(name, 0, ret)
-#define DEF_HELPER_1(name, ret, t1)                     DEF_HELPER_FLAGS_1(name, 0, ret, t1)
-#define DEF_HELPER_2(name, ret, t1, t2)                 DEF_HELPER_FLAGS_2(name, 0, ret, t1, t2)
-#define DEF_HELPER_3(name, ret, t1, t2, t3)             DEF_HELPER_FLAGS_3(name, 0, ret, t1, t2, t3)
-#define DEF_HELPER_4(name, ret, t1, t2, t3, t4)         DEF_HELPER_FLAGS_4(name, 0, ret, t1, t2, t3, t4)
-#define DEF_HELPER_5(name, ret, t1, t2, t3, t4, t5)     DEF_HELPER_FLAGS_5(name, 0, ret, t1, t2, t3, t4, t5)
-#define DEF_HELPER_6(name, ret, t1, t2, t3, t4, t5, t6) DEF_HELPER_FLAGS_6(name, 0, ret, t1, t2, t3, t4, t5, t6)
+#define DEF_HELPER_0(name, ret)                             DEF_HELPER_FLAGS_0(name, 0, ret)
+#define DEF_HELPER_1(name, ret, t1)                         DEF_HELPER_FLAGS_1(name, 0, ret, t1)
+#define DEF_HELPER_2(name, ret, t1, t2)                     DEF_HELPER_FLAGS_2(name, 0, ret, t1, t2)
+#define DEF_HELPER_3(name, ret, t1, t2, t3)                 DEF_HELPER_FLAGS_3(name, 0, ret, t1, t2, t3)
+#define DEF_HELPER_4(name, ret, t1, t2, t3, t4)             DEF_HELPER_FLAGS_4(name, 0, ret, t1, t2, t3, t4)
+#define DEF_HELPER_5(name, ret, t1, t2, t3, t4, t5)         DEF_HELPER_FLAGS_5(name, 0, ret, t1, t2, t3, t4, t5)
+#define DEF_HELPER_6(name, ret, t1, t2, t3, t4, t5, t6)     DEF_HELPER_FLAGS_6(name, 0, ret, t1, t2, t3, t4, t5, t6)
+#define DEF_HELPER_7(name, ret, t1, t2, t3, t4, t5, t6, t7) DEF_HELPER_FLAGS_7(name, 0, ret, t1, t2, t3, t4, t5, t6, t7)
 
 /* MAX_OPC_PARAM_IARGS must be set to n if last entry is DEF_HELPER_FLAGS_n. */
 
