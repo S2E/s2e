bcf00c521fadb088a4107e8ae3eed05e0ee6db0b
Vitaly Chipounov <vitaly@chipounov.fr>
2025-08-25 17:11:35 +0000

    klee/ref: imported use-after-free fixes from upstream
    Signed-off-by: Vitaly Chipounov <vitaly@chipounov.fr>

--- klee/include/klee/util/Ref.h
+++ klee/include/klee/util/Ref.h
@@ -62,6 +62,11 @@ public:
         inc();
     }
 
+    // normal move constructor: invoke the move assignment operator
+    ref(ref<T> &&r) noexcept : ptr(nullptr) {
+        *this = std::move(r);
+    }
+
     // conversion constructor
     template <class U> ref(const ref<U> &r) : ptr(r.ptr) {
         inc();
@@ -76,17 +81,71 @@ public:
      * despite a redundant template. */
     ref<T> &operator=(const ref<T> &r) {
         r.inc();
+        // Create a copy of the pointer as the
+        // referenced object might get destroyed by the following dec(),
+        // like in the following example:
+        // ````````````````````````
+        //    Expr {
+        //        ref<Expr> next;
+        //    }
+        //
+        //    ref<Expr> root;
+        //    root = root->next;
+        // ````````````````````````
+        T *saved_ptr = r.ptr;
         dec();
-        ptr = r.ptr;
+        ptr = saved_ptr;
 
         return *this;
     }
 
     template <class U> ref<T> &operator=(const ref<U> &r) {
         r.inc();
+        // Create a copy of the pointer as the currently
+        // referenced object might get destroyed by the following dec(),
+        // like in the following example:
+        // ````````````````````````
+        //    Expr {
+        //        ref<Expr> next;
+        //    }
+        //
+        //    ref<Expr> root;
+        //    root = root->next;
+        // ````````````````````````
+
+        U *saved_ptr = r.ptr;
+        dec();
+        ptr = saved_ptr;
+
+        return *this;
+    }
+
+    // Move assignment operator
+    ref<T> &operator=(ref<T> &&r) noexcept {
+        if (this == &r)
+            return *this;
         dec();
         ptr = r.ptr;
+        r.ptr = nullptr;
+        return *this;
+    }
 
+    // Move assignment operator
+    template <class U> ref<T> &operator=(ref<U> &&r) noexcept {
+        if (static_cast<void *>(this) == static_cast<void *>(&r))
+            return *this;
+
+        // Do not swap as the types might be not compatible
+        // Decrement local counter to not hold reference anymore
+        dec();
+
+        // Assign to this ref
+        ptr = cast_or_null<T>(r.ptr);
+
+        // Invalidate old ptr
+        r.ptr = nullptr;
+
+        // Return this pointer
         return *this;
     }
 
